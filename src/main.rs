9a76dc91658f3fbd8959e74fa4e6e4de6bc104fc
addthoriq1304 <ibnumasli@gmail.com>
2025-01-30 14:50:43 +0700

    Query SQL fetch

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use std::time::Duration;
-    use sqlx::{postgres::PgPoolOptions, Connection, Error, PgConnection, Pool, Postgres};
+    use sqlx::{Connection, Error, PgConnection, Pool, Postgres, Row};
+    use sqlx::postgres::{PgPoolOptions, PgRow};
+    use futures::TryStreamExt;
 
     async fn get_pool() -> Result<Pool<Postgres>, Error> {
         let url = "postgres://postgres:123@localhost:5432/rust_db";
@@ -18,6 +20,83 @@ mod tests {
             .connect(url).await
     }
 
+
+    #[tokio::test]
+    // Untuk melakukan lazy load dengan menampilkan jutaan data
+    async fn test_fetch_stream() -> Result<(), Error> {
+        let pool = get_pool().await?;
+        let stmt = String::from("SELECT * FROM categories");
+        let mut result = sqlx::query(&stmt)
+            .fetch(&pool);
+
+        while let Some(row) = result.try_next().await? {
+            let id: String = row.get("id");
+            let name: String = row.get("name");
+            let desc: String = row.get("description");
+            println!("ID: {}, Name: {}, Desc: {}", id, name, desc);
+        }
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    // Menampilkan semua data baik kosong atau ada
+    async fn test_fetch_all() -> Result<(), Error> {
+        let pool = get_pool().await?;
+        let stmt = String::from("SELECT * FROM categories");
+        let result: Vec<PgRow> = sqlx::query(&stmt)
+            .fetch_all(&pool).await?;
+
+        for val in result {
+            let id: String = val.get("id");
+            let name: String = val.get("name");
+            let desc: String = val.get("description");
+
+            println!("ID: {}, Name: {}, Desc: {}", id, name, desc);
+        }
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    // Menampilkan data wajib ada, tidak boleh kosong
+    async fn test_fetch_one() -> Result<(), Error> {
+        let pool = get_pool().await?;
+        let stmt = String::from("SELECT * FROM categories WHERE id = $1");
+        let result: PgRow = sqlx::query(&stmt)
+            .bind("C")
+            .fetch_one(&pool).await?;
+
+        let id: String = result.get("id");
+        let name: String = result.get("name");
+        let desc: String = result.get("description");
+
+        println!("ID: {}, Name: {}, Desc: {}", id, name, desc);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    // Menampilkan data ada maupun kosong
+    async fn test_fetch_optional() -> Result<(), Error> {
+        let pool = get_pool().await?;
+        let stmt = String::from("SELECT * FROM categories WHERE id = $1");
+        let result: Option<PgRow> = sqlx::query(&stmt)
+            .bind("E")
+            .fetch_optional(&pool).await?;
+
+        if let Some(row) = result {
+            let id: String = row.get("id");
+            let name: String = row.get("name");
+            let desc: String = row.get("description");
+
+            println!("ID: {}, Name: {}, Desc: {}", id, name, desc)
+        } else {
+            println!("Data Not Found")
+        }
+
+        Ok(())
+    }
 
     #[tokio::test]
     async fn test_prepare_stmt_query_as_stmt_using_bind() -> Result<(), Error> {
