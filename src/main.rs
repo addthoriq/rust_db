9c3524750e9e208efc65a297a8ade280ddf3b2a9
addthoriq1304 <ibnumasli@gmail.com>
2025-01-30 10:40:48 +0700

    connection to db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use sqlx::{Connection, Error, PgConnection};
+
+    #[tokio::test]
+    async fn test_manual_connection() -> Result<(), Error> {
+        let url = "postgres://postgres:123@localhost:5432/rust_db";
+        let connection: PgConnection = PgConnection::connect(url).await?;
+
+        connection.close().await?;
+        Ok(())
+    }
+}
